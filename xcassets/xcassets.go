19d73c873e2cdd2f9956ca712b34f5064819d979
Patrick Molgaard <patrick@gleepost.com>
2015-03-18 19:18:20 +0000

    Add godoc comments

--- xcassets/xcassets.go
+++ xcassets/xcassets.go
@@ -5,11 +5,13 @@ import (
 	"strings"
 )
 
+//Contents represents a Contents.json file.
 type Contents struct {
 	Images []Image `json:"images"`
 	Info   Info    `json:"info"`
 }
 
+//Image is a particular image within an image set.
 type Image struct {
 	Size                 string `json:"size"`
 	Idiom                string `json:"idiom"`
@@ -21,6 +23,7 @@ type Image struct {
 	Orientation          string `json:"orientation,omitempty"`
 }
 
+//Px returns the dimensions in pixels of this image.
 func (i Image) Px() uint {
 	size := strings.Split(i.Size, "x")
 	sizePX, _ := strconv.ParseUint(size[0], 10, 32)
@@ -28,11 +31,13 @@ func (i Image) Px() uint {
 	return uint(sizePX * scale)
 }
 
+//Info contains metadata about this image set.
 type Info struct {
 	Version int    `json:"version"`
 	Author  string `json:"author"`
 }
 
+//NewContents creates a new Contents, containing no images.
 func NewContents() Contents {
 	contents := Contents{}
 	contents.Info = Info{
